147bc285ca2fe65ffea1f0bd27e35b93c2a59462
21beach12 <1621706503@qq.com>
2020-06-20 19:00:16 +0800

    1231asdqwe

--- ciserver.js
+++ ciserver.js
@@ -1,16 +1,15 @@
-//用nodeJS写webhook的post请求接受密码
+//用nodeJS写webhook的post请求接收代码
 const http=require('http')
 const child=require('child_process')
-
-var  server=http.createServer()
-server.on('request',function (req,res) {
-    res.end("success!!")
+var server=http.createServer()
+server.on('request',function (reg,res) {
+    res.end("sucess!")
     child.execFile('./cibuild.sh',function (error,stdout,stderr) {
         console.log((stdout))
     })
 })
-
 server.listen(3201,function (err) {
-    if(!err)
-    console.log("server start at port:"+3201)
+    if(!err){
+        console.log("server start at port:"+3201)
+    }
 })
